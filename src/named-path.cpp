30638e8c5ab99b214a092c5e7dd9b0c4b26866cd
Jeremy Moles <cubicool@gmail.com>
2014-12-09 16:28:19 -0500

    Fixes API bugs in name-path.cpp.

--- src/named-path.cpp
+++ src/named-path.cpp
@@ -4,7 +4,7 @@
 #include <list>
 #include <string>
 
-typedef std::list<cairo_path_t*>                          cairocks_named_path_list_t;
+typedef std::list<cairo_path_t*> cairocks_named_path_list_t;
 typedef std::map<std::string, cairocks_named_path_list_t> cairocks_named_path_t;
 
 static cairo_user_data_key_t PATH_DATA;
@@ -25,7 +25,13 @@ static void cairocks_named_path_private_destroy(void* data) {
 	delete static_cast<cairocks_named_path_t*>(data);
 }
 
-void cairocks_named_path_private_append(cairo_t* cr, const char* named_path, bool new_path=true) {
+extern "C" {
+
+cairo_bool_t cairocks_named_path_private_append(
+	cairo_t* cr,
+	const char* named_path,
+	bool new_path=true
+) {
 	cairocks_named_path_t* data = cairocks_named_path_private_get(cr);
 
 	if(!data) {
@@ -37,14 +43,16 @@ void cairocks_named_path_private_append(cairo_t* cr, const char* named_path, boo
 	(*data)[named_path].push_back(cairo_copy_path(cr));
 
 	if(new_path) cairo_new_path(cr);
+
+	return TRUE;
 }
 
-void cairocks_append_named_path(cairo_t* cr, const char* named_path) {
-	cairocks_named_path_private_append(cr, named_path);
+cairo_bool_t cairocks_append_named_path(cairo_t* cr, const char* named_path) {
+	return cairocks_named_path_private_append(cr, named_path);
 }
 
-void cairocks_append_named_path_preserve(cairo_t* cr, const char* named_path) {
-	cairocks_named_path_private_append(cr, named_path, false);
+cairo_bool_t cairocks_append_named_path_preserve(cairo_t* cr, const char* named_path) {
+	return cairocks_named_path_private_append(cr, named_path, false);
 }
 
 cairo_bool_t cairocks_set_named_path(cairo_t* cr, const char* named_path) {
@@ -67,11 +75,11 @@ cairo_bool_t cairocks_set_named_path(cairo_t* cr, const char* named_path) {
 	return TRUE;
 }
 
-void cairocks_remove_named_path(cairo_t* cr, const char* named_path) {
-	cairocks_named_path_t*          data      = cairocks_named_path_private_get(cr);
+cairo_bool_t cairocks_remove_named_path(cairo_t* cr, const char* named_path) {
+	cairocks_named_path_t* data = cairocks_named_path_private_get(cr);
 	cairocks_named_path_t::iterator key_value = data->find(named_path);
 
-	if(key_value == data->end()) return;
+	if(key_value == data->end()) return FALSE;
 
 	for(
 		cairocks_named_path_list_t::const_iterator i = key_value->second.begin();
@@ -80,5 +88,9 @@ void cairocks_remove_named_path(cairo_t* cr, const char* named_path) {
 	) cairo_path_destroy(*i);
 
 	(*data).erase(key_value);
+
+	return TRUE;
+}
+
 }
 
