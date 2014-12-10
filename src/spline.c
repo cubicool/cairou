8f920991f7ae7f42733a6f93d380c53b31b9f86a
Jeremy Moles <cubicool@gmail.com>
2014-12-10 17:22:45 -0500

    Spline function name cleanup.

--- src/spline.c
+++ src/spline.c
@@ -3,7 +3,7 @@
 
 #include "cairocks.h"
 
-static cairocks_point_t before(
+static cairocks_point_t cairocks_spline_private_before(
 	cairocks_point_t p0,
 	cairocks_point_t p1,
 	cairocks_point_t p2
@@ -16,7 +16,7 @@ static cairocks_point_t before(
 	return r;
 }
 
-static cairocks_point_t after(
+static cairocks_point_t cairocks_spline_private_after(
 	cairocks_point_t p0,
 	cairocks_point_t p1,
 	cairocks_point_t p2
@@ -29,7 +29,7 @@ static cairocks_point_t after(
 	return r;
 }
 
-static cairocks_point_t quadratic(
+static cairocks_point_t cairocks_spline_private_quadratic(
 	double t,
 	cairocks_point_t p0,
 	cairocks_point_t p1,
@@ -37,24 +37,15 @@ static cairocks_point_t quadratic(
 ) {
 	cairocks_point_t r;
 
-	r.x =
-		(1 - t) * (1 - t) * p0.x +
-		2 * (1 - t) * t * p1.x +
-		t * t * p2.x
-	;
-
-	r.y =
-		(1 - t) * (1 - t) * p0.y +
-		2 * (1 - t) * t * p1.y +
-		t * t * p2.y
-	;
+	r.x = (1.0 - t) * (1.0 - t) * p0.x + 2.0 * (1.0 - t) * t * p1.x + t * t * p2.x;
+	r.y = (1.0 - t) * (1.0 - t) * p0.y + 2.0 * (1.0 - t) * t * p1.y + t * t * p2.y;
 
 	return r;
 }
 
 #define GET_POINT(x) (points[(x) % n_points])
 
-static void get_neighbors(
+static void cairocks_spline_private_get_neighbors(
 	cairocks_point_t* points,
 	int n_points,
 	int closed,
@@ -64,16 +55,21 @@ static void get_neighbors(
 	cairocks_point_t* next
 ) {
 	/* For the first and last point of a non-closed spline, we make up 'previous'
-	 * and 'next' points by extrapolating a quadratic spline. */
+	 * and 'next' points by extrapolating a cairocks_spline_private_quadratic spline. */
 	if(!i) {
 		if(prev) {
 			if(closed) *prev = GET_POINT (n_points - 1);
 
 			else {
 				double t = -1.0 / 4.0;
-				cairocks_point_t c = before(GET_POINT(0), GET_POINT(1), GET_POINT(2));
 
-				*prev = quadratic(t, GET_POINT(0), c, GET_POINT(1));
+				cairocks_point_t c = cairocks_spline_private_before(
+					GET_POINT(0),
+					GET_POINT(1),
+					GET_POINT(2)
+				);
+
+				*prev = cairocks_spline_private_quadratic(t, GET_POINT(0), c, GET_POINT(1));
 			}
 		}
 
@@ -88,9 +84,19 @@ static void get_neighbors(
 
 			else {
 				double t = 5.0 / 4.0;
-				cairocks_point_t c = after(GET_POINT(i - 2), GET_POINT(i - 1), GET_POINT(i));
 
-				*next = quadratic(t, GET_POINT(n_points - 2), c, GET_POINT(n_points - 1));
+				cairocks_point_t c = cairocks_spline_private_after(
+					GET_POINT(i - 2),
+					GET_POINT(i - 1),
+					GET_POINT(i)
+				);
+
+				*next = cairocks_spline_private_quadratic(
+					t,
+					GET_POINT(n_points - 2),
+					c,
+					GET_POINT(n_points - 1)
+				);
 			}
 		}
 	}
@@ -125,11 +131,28 @@ cairo_bool_t cairocks_append_spline(
 		cairocks_point_t c1;
 		cairocks_point_t c2;
 
-		get_neighbors(points, n_points, closed, i, &this, &prev, &next);
-		get_neighbors(points, n_points, closed, i - 1, NULL, &prevprev, NULL);
+		cairocks_spline_private_get_neighbors(
+			points,
+			n_points,
+			closed,
+			i,
+			&this,
+			&prev,
+			&next
+		);
 
-		c1 = after(prevprev, prev, this);
-		c2 = before(prev, this, next);
+		cairocks_spline_private_get_neighbors(
+			points,
+			n_points,
+			closed,
+			i - 1,
+			NULL,
+			&prevprev,
+			NULL
+		);
+
+		c1 = cairocks_spline_private_after(prevprev, prev, this);
+		c2 = cairocks_spline_private_before(prev, this, next);
 
 		cairo_curve_to(
 			cr,
