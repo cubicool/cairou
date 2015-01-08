35b2de39bbf92c997c76b1735b8a784dfc1a4288
Jeremy Moles <cubicool@gmail.com>
2015-01-08 10:25:33 -0500

    Large testing changes; see details.
    This patch begins the process of adding newline/breaking support to the
    cairocks text functions (still incomplete). It wholesale adds the
    utf8cpp library to assist in dealing with unicode. Further, this patch
    modifies some of the testing scripts, though they remain in flux.

--- src/text.cpp
+++ src/text.cpp
@@ -1,8 +1,84 @@
 #include "cairocks.h"
+#include "utf8.h"
 
-#include <stdio.h>
+#include <cstring>
+#include <vector>
 
-static cairo_bool_t cairocks_text_private_init(
+// Since we use the variable name "utf8" a great deal, we need to re-assign
+// the namespace defined by this header above to something more compatible with
+// our code.
+namespace UTF8 = utf8;
+
+// TODO: REMOVE ME!
+#include <iostream>
+
+// This structure holds all the data we need to easily account for newlines in
+// blocks of text and to use those substrings (as delimited by those newlines)
+// with the Cairo API. We take a fairly easy approach wherein we essentially
+// break the string up into small strings by storing a start position for each
+// substring and replacing the newlines we find in our copy with NULL terminators.
+//
+// TODO: Convert the next()'ing idiom into the standard C++ iterator API.
+class cairocks_text_private_t {
+public:
+	typedef std::vector<unsigned int> starting_points;
+
+	cairocks_text_private_t(const char* utf8_ro):
+	utf8(NULL) {
+		unsigned int size = std::strlen(utf8_ro) + 1;
+
+		// Allocate our copy of the UTF8 string.
+		utf8 = new char[size];
+
+		std::strcpy(utf8, utf8_ro);
+
+		unsigned int pos = 0;
+		unsigned int last = 0;
+		const char* c = utf8_ro;
+		const char* end = utf8_ro + size;
+
+		while(c < end) {
+			if(UTF8::next(c, end) == '\n') {
+				starts.push_back(last);
+
+				utf8[pos] = 0;
+
+				last = pos + 1;
+			}
+
+			pos++;
+		}
+
+		n = starts.begin();
+	}
+
+	~cairocks_text_private_t() {
+		if(utf8) delete[] utf8;
+	}
+
+	const char* next() {
+		if(n == starts.end()) {
+			n = starts.begin();
+
+			return NULL;
+		}
+
+		const char* str = static_cast<const char*>(&utf8[*n]);
+
+		n++;
+
+		return str;
+	}
+
+private:
+	char* utf8;
+	starting_points starts;
+	starting_points::const_iterator n;
+};
+
+// Initializes the Cairo state for the passed-in context and returns a private
+// structure containing the data necessary to properly draw the string.
+static cairocks_text_private_t* cairocks_text_private_init(
 	cairo_t*              cr,
 	const char*           utf8,
 	const char*           font,
@@ -18,22 +94,19 @@ static cairo_bool_t cairocks_text_private_init(
 	);
 
 	cairo_set_font_size(cr, size);
-
-	/* Get the extents, so we can use them to properly position our text.
-	 * TODO: We need to find all \n occurances, and properly break/extent these
-	 * individual chunks. */
 	cairo_text_extents(cr, utf8, extents);
 
-	/* printf(">> %5.2f %5.2f %5.2f %5.2f %5.2f %5.2f\n",
-		extents->x_bearing,
-		extents->y_bearing,
-		extents->width,
-		extents->height,
-		extents->x_advance,
-		extents->y_advance
-	); */
+	cairocks_text_private_t* data = NULL;
 
-	return TRUE;
+	try {
+		data = new cairocks_text_private_t(utf8);
+	}
+
+	catch(...) {
+		data = NULL;
+	}
+
+	return data;
 }
 
 static void cairocks_text_private_get_translate(
@@ -74,8 +147,14 @@ static cairo_bool_t cairocks_text_private_draw(
 
 	cairo_text_extents_t extents;
 
-	cairocks_text_private_init(cr, utf8, font, size, flags, &extents);
+	cairocks_text_private_t* text = cairocks_text_private_init(cr, utf8, font, size, flags, &extents);
+
 	cairocks_text_private_get_translate(flags, &extents, &tx, &ty);
+
+	// TODO: Print shit...
+	for(const char* str = text->next(); str; str = text->next()) {
+		std::cout << str << std::endl;
+	}
 
 	cairo_save(cr);
 	cairo_translate(cr, x + tx, y + ty);
@@ -84,8 +163,12 @@ static cairo_bool_t cairocks_text_private_draw(
 
 	cairo_restore(cr);
 
+	delete text;
+
 	return TRUE;
 }
+
+extern "C" {
 
 cairo_bool_t cairocks_show_text(
 	cairo_t*    cr,
@@ -125,8 +208,8 @@ cairo_bool_t cairocks_text_extents(
 	cairocks_text_private_init(cr, utf8, font, size, flags, extents);
 
 	if(rect_extents) {
-		double rx;
-		double ry;
+		double rx = 0.0;
+		double ry = 0.0;
 
 		/* Calculate the rect X origin. */
 		if(flags & CAIROCKS_X_LEFT) rx = 0.0;
@@ -146,10 +229,6 @@ cairo_bool_t cairocks_text_extents(
 
 		else ry = -extents->y_bearing;
 
-		/* cairocks_text_private_get_translate(flags, extents, &tx, &ty);
-		cairo_save(cr);
-		cairo_translate(cr, x + tx, y + ty); */
-
 		/* Finally, the width and height of the rect always match the extent
 		 * width and height. */
 		rect_extents[0] = x - rx;
@@ -160,3 +239,6 @@ cairo_bool_t cairocks_text_extents(
 
 	return TRUE;
 }
+
+}
+
