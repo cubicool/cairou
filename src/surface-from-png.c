321f6536f3bc63443d7e74a6b182c0a234201855
Jeremy Moles <cubicool@gmail.com>
2015-01-20 14:45:56 -0500

    Formatting fixes for PNG loader.

--- src/surface-from-png.c
+++ src/surface-from-png.c
@@ -3,14 +3,14 @@
 #include <string.h>
 
 typedef struct {
-	unsigned int   last;
+	unsigned int last;
 	unsigned char* data;
 } cairocks_read_png_closure_t;
 
 static cairo_status_t cairocks_read_png(
-	void*          closure,
+	void* closure,
 	unsigned char* data,
-	unsigned int   length
+	unsigned int length
 ) {
 	cairocks_read_png_closure_t* c = (cairocks_read_png_closure_t*)(closure);
 
@@ -22,7 +22,7 @@ static cairo_status_t cairocks_read_png(
 }
 
 cairo_surface_t* cairocks_surface_from_png_data(unsigned char* data) {
-	cairo_surface_t*            surface;
+	cairo_surface_t* surface;
 	cairocks_read_png_closure_t closure;
 
 	closure.last = 0;
